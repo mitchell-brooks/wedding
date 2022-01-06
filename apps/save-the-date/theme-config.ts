fc0fb6aef1fc2bfeffde1d5bcdbfc708a027ac6a
Mitchell Brooks <mitchellbrooks@gmail.com>
2022-01-05 23:15:14 -0500

    :fire: Remove babelrc to default to SWC compiler
    - Remove babelrc which was used to remove styled-components error in development

--- apps/save-the-date/theme-config.ts
+++ apps/save-the-date/theme-config.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 /* eslint-disable-next-line */
 export interface GlobalStylesProps {}
